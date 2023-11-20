2e3a9b0f9c6af4d6d965f24729f9a71271413da9
BilalMern <bilal.mern@gmail.com>
2023-11-20 12:36:29 +0500

    basics

--- Intro/app.js
+++ Intro/app.js
@@ -1,2 +1,37 @@
 // Variable:
 // variable reserve a space in memory in which the value we assign can be store in it.
+
+// for (let i = 1; i <=10; i++){
+//     document.write("2" + "x" + i + "=" + 2*i + "<br>")
+// }
+
+// let arr = ["bilal","qaiser","amir"]
+// for(let i =0;i < arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// let cities=["karachi","islamabad","lahore"]
+// for(let i = 0; i < cities.length; i++){
+//     if(cities[i]==="karachi"){
+//         console.log(cities[i] + " " + "is a cleanest city")
+// break;
+// }
+// }
+
+//Stopping infinite loop by using break:
+// for(let i =1; i >0; i++){
+//     if(i===11){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+
+// nested loop
+
+// for(let i=0; i<5; i++){
+//     for(let j=0;j<3;j++){
+//         console.log("pakistan zindabad")
+//     }
+// }
