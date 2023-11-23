f84feb60265e9c74bb192babd1a6dbaf77aa1166
BilalMern <bilal.mern@gmail.com>
2023-11-23 16:00:42 +0500

    Bulb on off

---
+++ Intro/Bulb on and off/app.js
@@ -0,0 +1,9 @@
+function onBulb(){
+let bulb= document.getElementById("bulb")
+bulb.src="https://images.pexels.com/photos/12700577/pexels-photo-12700577.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"  
+}
+
+function offBulb(){
+    let bulb= document.getElementById("bulb")
+    bulb.src="https://images.pexels.com/photos/11025320/pexels-photo-11025320.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" 
+}
