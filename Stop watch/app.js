8ab623f5ae88ccc19c74288216c8231bfa14ccb0
BilalMern <bilal.mern@gmail.com>
2023-11-23 19:40:14 +0500

    disable functionality in watch

--- Stop watch/app.js
+++ Stop watch/app.js
@@ -9,41 +9,41 @@
 //     clearInterval(interval)
 // },5000)
 
-
 //Stop Watch:
-let min =0;
-let sec =0;
+let min = 0;
+let sec = 0;
 let msec = 0;
 let interval;
-let mins= document.getElementById("min");
-let secs= document.getElementById("sec");
-let msecs= document.getElementById("msec");
-function timer(){
-    msec++;
-    msecs.innerHTML = msec;
-    if(msec>=100){
-        sec++;
-        secs.innerHTML=sec;
-        msec=0;
-    }else if(sec>=60){
-        min++;
-        mins.innerHTML = min;
-        sec = 0;
-    }
-}
-function start(){
-interval = setInterval(timer,10)
-}
-function stop(){
-    clearInterval(interval)
-}
-function reset(){
-    min = 0;
-    sec = 0;
-    msec =0;
-    mins.innerHTML = min;
+let mins = document.getElementById("min");
+let secs = document.getElementById("sec");
+let msecs = document.getElementById("msec");
+function timer() {
+  msec++;
+  msecs.innerHTML = msec;
+  if (msec >= 100) {
+    sec++;
     secs.innerHTML = sec;
-    msecs.innerHTML = msec;
-    stop();
-
+    msec = 0;
+  } else if (sec >= 60) {
+    min++;
+    mins.innerHTML = min;
+    sec = 0;
+  }
+}
+function start() {
+  interval = setInterval(timer, 10);
+  document.getElementById("startBtn").disabled =true;
+}
+function stop() {
+  clearInterval(interval);
+}
+function reset() {
+  min = 0;
+  sec = 0;
+  msec = 0;
+  mins.innerHTML = min;
+  secs.innerHTML = sec;
+  msecs.innerHTML = msec;
+  stop();
+  document.getElementById("startBtn").disabled =false;
 }
