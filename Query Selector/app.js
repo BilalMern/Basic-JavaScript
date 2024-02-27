2abb1422bb5ca45da05c33e8f7c51e0a87064e24
BilalMern <bilal.mern@gmail.com>
2024-02-27 19:34:57 +0500

    advance selector

--- Query Selector/app.js
+++ Query Selector/app.js
@@ -1,6 +1,9 @@
-let query=document.querySelector(".nav")
+let query=document.querySelector(".nav").innerHTML = "hello there!"
 console.log(query)
 
 
 let hello = document.querySelectorAll(".nav")[1].innerHTML;
 console.log(hello)
+
+let advanceSelector = document.querySelectorAll("#heading h1")[1].innerHTML
+console.log(advanceSelector)
