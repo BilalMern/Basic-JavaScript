0b926529009659b3c11d27656bc883fe83d4717d
BilalMern <bilal.mern@gmail.com>
2024-02-27 19:28:35 +0500

    queryselectAll

--- Query Selector/app.js
+++ Query Selector/app.js
@@ -1,2 +1,6 @@
-let query=document.querySelector("#nav").getAttribute("class")
+let query=document.querySelector(".nav")
 console.log(query)
+
+
+let hello = document.querySelectorAll(".nav")[1].innerHTML;
+console.log(hello)
