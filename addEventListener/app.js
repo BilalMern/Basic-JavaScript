6f8ca232f3b14e4fad93eb6bc9f2404ea9c658a1
BilalMern <bilal.mern@gmail.com>
2024-03-07 16:07:47 +0500

    updated

--- addEventListener/app.js
+++ addEventListener/app.js
@@ -1,5 +1,21 @@
-let nav = document.querySelector("#nav").onclick=abc
+// let nav = document.querySelector("#nav").innerHTML=`<h1>Hello there</h1>`
+// document.querySelector("#nav").onclick=abc
+// function abc(){
+//    document.querySelector("#nav").style.color="red"
+// }
 
-function abc(){
-   document.querySelector("#nav").style.color="red"
+// let nav = document.getElementById("nav");
+// nav.innerHTML=`<h1>Hello there</h1>`
+// nav.onclick=abc;
+// function abc (){
+//     nav.style.background="red"
+// }
+
+let nav = document.querySelector("#nav");
+nav.innerHTML=`<h1>Hello there</h1>`
+nav.addEventListener("click",abc);
+function abc (){
+    nav.style.background="red"
 }
+
+
